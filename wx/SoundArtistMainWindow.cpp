1e2d99047e1d33cc5a76678135e20cb59ac34f8c
Brandon Aubie <brandon@aubie.ca>
2011-06-21 11:51:02 -0400

    Add derived classes

---
+++ wx/SoundArtistMainWindow.cpp
@@ -0,0 +1,1 @@
+#include "SoundArtistMainWindow.h"SoundArtistMainWindow::SoundArtistMainWindow( wxWindow* parent ):MainWindow( parent ){}void SoundArtistMainWindow::OnQuit( wxCommandEvent& event ){// TODO: Implement OnQuit}
